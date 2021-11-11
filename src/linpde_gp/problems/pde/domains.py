86b2e7f380d964cc77d5bdb3c7ae41a162743853
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2021-11-11 18:30:29 +0100

    Improve `Domain` implementation

--- src/linpde_gp/problems/pde/domains.py
+++ src/linpde_gp/problems/pde/domains.py
@@ -1,13 +1,16 @@
 import abc
-from typing import Iterable, List, Set, Tuple, Union
+import functools
+import operator
+from collections.abc import Sequence
+from typing import Union
 
 import numpy as np
 import probnum as pn
 from numpy import typing as npt
-from probnum.typing import ArrayLike, FloatArgType, IntArgType
+from probnum.typing import ArrayLike, FloatArgType, ShapeArgType, ShapeType
 
 DomainLike = Union[
-    "Domain", Tuple[FloatArgType, FloatArgType], List[FloatArgType], Set[ArrayLike]
+    "Domain", tuple[FloatArgType, FloatArgType], list[FloatArgType], ArrayLike
 ]
 
 
@@ -16,32 +19,38 @@ def asdomain(arg: DomainLike) -> "Domain":
         return arg
     elif isinstance(arg, (tuple, list)) and len(arg) == 2:
         return Interval(float(arg[0]), float(arg[1]))
-    elif isinstance(arg, set):
-        return PointSet(arg)
     else:
-        raise TypeError(f"`{arg}` could not be converted into a `Domain` object")
+        return Point(arg)
 
 
 class Domain(abc.ABC):
-    def __init__(self, dim: IntArgType, dtype: npt.DTypeLike) -> None:
-        self._dim = int(dim)
+    def __init__(self, shape: ShapeArgType, dtype: npt.DTypeLike) -> None:
+        self._shape = pn.utils.as_shape(shape)
         self._dtype = np.dtype(dtype)
-
-    @property
-    def dim(self) -> int:
-        return self._dim
 
     @property
     def dtype(self) -> np.dtype:
         return self._dtype
 
     @property
+    def shape(self) -> ShapeType:
+        return self._shape
+
+    @property
+    def ndims(self) -> int:
+        return len(self.shape)
+
+    @property
+    def size(self) -> int:
+        return functools.reduce(operator.mul, self.shape, 1)
+
+    @property
     @abc.abstractmethod
-    def boundary(self) -> "Domain":
+    def boundary(self) -> Sequence["Domain"]:
         pass
 
 
-class Interval(Domain):
+class Interval(Domain, Sequence):
     def __init__(
         self,
         lower_bound: FloatArgType,
@@ -61,7 +70,10 @@ class Interval(Domain):
 
         assert self._lower_bound.dtype == self._upper_bound.dtype
 
-        super().__init__(dim=1, dtype=self._lower_bound.dtype)
+        super().__init__(shape=(), dtype=self._lower_bound.dtype)
+
+    def __len__(self) -> int:
+        return 2
 
     def __getitem__(self, idx: int) -> np.floating:
         if idx in (0, -2):
@@ -76,30 +88,42 @@ class Interval(Domain):
         yield self._upper_bound
 
     @property
-    def boundary(self) -> "Domain":
-        return PointSet(list(self))
+    def boundary(self) -> Sequence["Domain"]:
+        return (Point(self._lower_bound), Point(self._upper_bound))
+
+    def __repr__(self) -> str:
+        return (
+            f"<Interval {[self._lower_bound, self._upper_bound]} with "
+            f"shape={self.shape} and "
+            f"dtype={str(self.dtype)}>"
+        )
+
+    def __array__(self) -> np.ndarray:
+        return np.hstack((self._lower_bound, self._upper_bound))
 
 
-class PointSet(Domain):
-    def __init__(self, points: Iterable[ArrayLike]) -> None:
-        self._points = list(points)
+class Point(Domain):
+    def __init__(self, point: ArrayLike) -> None:
+        self._point = np.asarray(point)
 
-        assert all(point.ndim in (0, 1) for point in self._points)
-        assert all(point.size == self._points[0].size for point in self._points)
-        assert all(point.dtype == self._points[0].dtype for point in self._points)
-
-        super().__init__(dim=self._points[0].size, dtype=self._points[0].dtype)
-
-    def __len__(self) -> int:
-        return len(self._points)
-
-    def __getitem__(self, idx: int) -> Union[np.ndarray, np.floating]:
-        self._points[idx]
-
-    def __iter__(self):
-        for point in self._points:
-            yield point
+        super().__init__(shape=self._point.shape, dtype=self._point.dtype)
 
     @property
-    def boundary(self) -> "Domain":
-        raise NotImplementedError()
+    def boundary(self) -> Sequence["Domain"]:
+        return ()
+
+    def __repr__(self) -> str:
+        return (
+            f"<Point {str(self._point)} with "
+            f"shape={self.shape} and "
+            f"dtype={str(self.dtype)}>"
+        )
+
+    def __array__(self, dtype: np.dtype = None) -> np.ndarray:
+        return np.array(self._point, dtype=dtype, copy=True)
+
+    def __float__(self):
+        if self.ndims > 1:
+            raise NotImplementedError()
+
+        return float(self._point)
