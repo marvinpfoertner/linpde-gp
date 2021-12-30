4c7cd68c11ca265815040f2e297782dec13a5720
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2021-12-30 17:48:41 +0100

    Add `HeatOperator`

--- src/linpde_gp/problems/pde/diffops.py
+++ src/linpde_gp/problems/pde/diffops.py
@@ -25,3 +25,35 @@ class LaplaceOperator:
     @functools.singledispatchmethod
     def project(self, basis: "linpde_gp.bases.Basis") -> pn.linops.LinearOperator:
         raise NotImplementedError()
+
+
+class HeatOperator:
+    def __call__(self, f: JaxFunction, argnum: int = 0) -> JaxFunction:
+        @jax.jit
+        def _f(t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs) -> jnp.ndarray:
+            tx = jnp.concatenate((t[None], x), axis=0)
+
+            return f(
+                *args[:argnum],
+                tx,
+                *args[argnum + 1 :],
+                **kwargs,
+            )
+
+        @jax.jit
+        def _f_heat(*args, **kwargs) -> jnp.ndarray:
+            tx = args[argnum]
+            t, x = tx[0], tx[1:]
+
+            args = args[:argnum] + args[argnum + 1 :]
+
+            df_dt = jax.grad(_f, argnums=0)(t, x, *args, **kwargs)[0]
+            laplace_f = laplace(_f, argnum=1)(t, x, *args, **kwargs)
+
+            return df_dt - laplace_f
+
+        return _f_heat
+
+    @functools.singledispatchmethod
+    def project(self, basis: "linpde_gp.bases.Basis") -> pn.linops.LinearOperator:
+        raise NotImplementedError()
