a705c25e76be5334a9b82586c3b73014173bb921
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-01-06 18:26:37 +0100

    Implement the PosteriorGaussianProcess

--- src/linpde_gp/randprocs/__init__.py
+++ src/linpde_gp/randprocs/__init__.py
@@ -1,3 +1,4 @@
 from . import _jax, kernels, mean_fns
 from ._function import Function
+from ._gaussian_process import PosteriorGaussianProcess
 from ._linear_transform import LinearTransformGaussianProcess
--- src/linpde_gp/randprocs/_gaussian_process.py
+++ src/linpde_gp/randprocs/_gaussian_process.py
@@ -1,52 +1,77 @@
 from collections.abc import Sequence
-from typing import Optional
 
+import jax
 import jax.numpy as np
 import probnum as pn
+import scipy.linalg
 
 from . import _jax
 
 
 class PosteriorGaussianProcess(pn.randprocs.GaussianProcess):
+    @classmethod
+    def from_measurements(
+        cls, prior: pn.randprocs.GaussianProcess, X: np.ndarray, fX: np.ndarray
+    ):
+        mX = prior._meanfun(X)
+        kXX = prior._covfun.jax(X[:, None, :], X[None, :, :])
+        kXX_cho = jax.scipy.linalg.cho_factor(kXX)
+
+        representer_weights = jax.scipy.linalg.cho_solve(kXX_cho, (fX - mX))
+
+        return cls(
+            prior=prior,
+            locations=[X],
+            measurements=[fX],
+            cross_covariances=[prior._covfun],
+            gram_matrices=[[kXX]],
+            representer_weights=representer_weights,
+        )
+
     def __init__(
         self,
         prior: pn.randprocs.RandomProcess,
-        representer_weights: Sequence[np.ndarray],
-        cross_covariances: Sequence[Optional[pn.randprocs.kernels.Kernel]],
+        locations: Sequence[np.ndarray],
+        measurements: Sequence[np.ndarray],
+        cross_covariances: Sequence[pn.randprocs.kernels.Kernel],
         gram_matrices: Sequence[Sequence[np.ndarray]],
+        representer_weights: np.ndarray,
     ):
         self._prior = prior
 
-        self._representer_weights = representer_weights
-        self._cross_covariances = cross_covariances
+        self._locations = tuple(locations)
+        self._measurements = tuple(measurements)
 
-        self._gram_matrices = gram_matrices
+        self._cross_covariances = tuple(cross_covariances)
+        self._cross_covariance = lambda x: np.hstack(
+            k_cross.jax(x, X)
+            for k_cross, X in zip(self._cross_covariances, self._locations)
+        )
 
-        self._alpha = np.vstack(self._representer_weights)
+        self._gram_matrices = tuple(tuple(row) for row in gram_matrices)
         self._gram_matrix = np.block(
             [
                 [
-                    self._gram_matrices[i][j] if i >= j else self._gram_matrices[j][i]
-                    for j in range(len(self._representer_weights))
+                    self._gram_matrices[i][j] if i >= j else self._gram_matrices[j][i].T
+                    for j in range(len(self._locations))
                 ]
-                for i in range(len(self._representer_weights))
+                for i in range(len(self._locations))
             ]
         )
-        self._cross_covariance = PosteriorGaussianProcess.CrossCovariance(
-            self._prior.input_dim,
-            self._cross_covariances,
-        )
+        self._gram_matrix_cholesky = scipy.linalg.cho_factor(self._gram_matrix)
+
+        self._representer_weights = representer_weights
 
         super().__init__(
             mean=PosteriorGaussianProcess.Mean(
                 prior_mean=self._prior._meanfun,
-                representer_weights=self._alpha,
                 cross_covariance=self._cross_covariance,
+                representer_weights=self._representer_weights,
             ),
             cov=PosteriorGaussianProcess.Kernel(
                 prior_kernel=self._prior._covfun,
-                gram_matrix_cholesky=None,
                 cross_covariance=self._cross_covariance,
+                gram_matrix_cholesky=self._gram_matrix_cholesky,
             ),
         )
 
@@ -54,20 +79,88 @@ class PosteriorGaussianProcess(pn.randprocs.GaussianProcess):
         def __init__(
             self,
             prior_mean: _jax.JaxMean,
+            cross_covariance,
             representer_weights: np.ndarray,
-            cross_covariance: "PosteriorGaussianProcess.CrossCovariance",
         ):
-            pass
+            self._prior_mean = prior_mean
+            self._representer_weights = representer_weights
+            self._cross_covariance = cross_covariance
+
+            super().__init__(m=self._call, vectorize=True)
+
+        def _call(self, x):
+            mx = self._prior_mean(x)
+            kLadj_xX = self._cross_covariance(x)
+
+            return mx + kLadj_xX @ self._representer_weights
 
     class Kernel(_jax.JaxKernel):
         def __init__(
             self,
             prior_kernel: _jax.JaxKernel,
+            cross_covariance,
             gram_matrix_cholesky,
-            cross_covariance: "PosteriorGaussianProcess.CrossCovariance",
         ):
-            pass
+            self._prior_kernel = prior_kernel
+            self._gram_matrix_cholesky = gram_matrix_cholesky
+            self._cross_covariance = cross_covariance
 
-    class CrossCovariance(_jax.JaxKernel):
-        def __init__(self, input_dim: int, cross_covariances):
-            pass
+            super().__init__(
+                self._call,
+                input_dim=self._prior_kernel.input_dim,
+                vectorize=True,
+            )
+
+        def _call(self, x0, x1):
+            k_xx = self._prior_kernel.jax(x0, x1)
+            kLadj_xX = self._cross_covariance(x0)
+            LkLadj_XX_cho = self._gram_matrix_cholesky
+            Lk_Xx = self._cross_covariance(x1)
+
+            return k_xx + kLadj_xX @ jax.scipy.linalg.cho_solve(LkLadj_XX_cho, Lk_Xx)
+
+    def condition_on_observations(self, X, fX):
+        k_X_Xprevs = tuple(
+            k_cross.jax(X[:, None, :], X_prev[None, :, :])
+            for k_cross, X_prev in zip(self._cross_covariances, self._locations)
+        )
+        kXX = self._prior._covfun.jax(X[:, None, :], X[None, :, :])
+        new_gram_row = k_X_Xprevs + (kXX,)
+
+        C = np.hstack(k_X_Xprevs)
+        new_representer_weights = _schur_update(
+            A_cho=self._gram_matrix_cholesky,
+            B=C.T,
+            C=C,
+            D=kXX,
+            A_inv_u=self._representer_weights,
+            v=(fX - self._prior._meanfun(X)),
+        )
+
+        return PosteriorGaussianProcess(
+            self._prior,
+            locations=self._locations + (X,),
+            measurements=self._measurements + (fX,),
+            cross_covariances=self._cross_covariances + (self._prior._covfun,),
+            gram_matrices=self._gram_matrices + (new_gram_row,),
+            representer_weights=new_representer_weights,
+        )
+
+
+def _schur_update(A_cho, B, C, D, A_inv_u, v):
+    """
+    This function solves the linear system
+
+    [[A, B], @ [[x], = [[u],
+     [C, D]]    [y]]    [v]]
+
+    given the Cholesky factor of A, B, C, D, A^{-1} u, and v.
+    """
+    A_inv_B = jax.scipy.linalg.cho_solve(A_cho, B)
+
+    S = D - C @ A_inv_B
+
+    y = jax.scipy.linalg.solve(S, v - C @ A_inv_u)
+    x = A_inv_u - A_inv_B @ y
+
+    return np.concatenate((x, y))
