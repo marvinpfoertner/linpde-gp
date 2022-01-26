153539196eca23faa06ce13dd96a0f1cf571702b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-01-26 18:26:23 +0100

    Square exponential kernel

--- src/linpde_gp/randprocs/kernels.py
+++ src/linpde_gp/randprocs/kernels.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 import jax
 import numpy as np
 import probnum as pn
+from jax import numpy as jnp
+from probnum.typing import ArrayLike
 
 
 class JaxKernel(pn.randprocs.kernels.Kernel):
@@ -23,3 +27,25 @@ class JaxKernel(pn.randprocs.kernels.Kernel):
     @property
     def jax(self):
         return self._k
+
+
+class ExpQuad(JaxKernel):
+    def __init__(self, input_dim: int, lengthscales: ArrayLike, output_scale: float):
+        self._lengthscales = np.asarray(lengthscales, dtype=np.double)
+        self._output_scale = np.asarray(output_scale, dtype=np.double)
+
+        super().__init__(self._jax, input_dim, vectorize=False)
+
+    def _evaluate(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> np.ndarray:
+        if x1 is None:
+            x1 = x0
+
+        square_dists = np.sum((x0 - x1) / self._lengthscales, axis=-1)
+
+        return self._output_scale * np.exp(-0.5 * square_dists)
+
+    @jax.jit(static_argnums=0)
+    def _jax(self, x0: jnp.ndarray, x1: jnp.ndarray) -> jnp.ndarray:
+        square_dists = jnp.sum((x0 - x1) / self._lengthscales, axis=-1)
+
+        return self._output_scale * jnp.exp(-0.5 * square_dists)
