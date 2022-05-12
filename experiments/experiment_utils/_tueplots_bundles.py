7804f4a04da4e455badf1de90a01f76d9244be1b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-05-12 13:35:52 +0200

    Update 1D CPU Experiment plots

--- experiments/experiment_utils/_tueplots_bundles.py
+++ experiments/experiment_utils/_tueplots_bundles.py
@@ -1,11 +1,12 @@
 from tueplots import figsizes, fonts, fontsizes
 
 
-def jmlr(*, rel_width=1.0, nrows=1, ncols=1, family="serif"):
+def jmlr(*, rel_width=1.0, nrows=1, ncols=1, family="serif", **kwargs):
     size = figsizes.jmlr2001(
         rel_width=rel_width,
         nrows=nrows,
         ncols=ncols,
+        **kwargs,
     )
     font_config = fonts.jmlr2001_tex(family=family)
     fontsize_config = fontsizes.jmlr2001()
