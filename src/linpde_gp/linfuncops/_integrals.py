ff0d0735f3a19ccfb8dcfa1e6bac7d551d74965c
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-04 21:31:17 +0200

    `UndefinedLebesgueIntegral` fallback

--- src/linpde_gp/linfuncops/_integrals.py
+++ src/linpde_gp/linfuncops/_integrals.py
@@ -1,5 +1,7 @@
 import functools
 
+import numpy as np
+import probnum as pn
 from probnum.typing import FloatLike
 
 from linpde_gp.functions import Affine, Constant
@@ -19,7 +21,33 @@ class UndefinedLebesgueIntegral(_linfuncop.LinearFunctionOperator):
 
     @functools.singledispatchmethod
     def __call__(self, f, /, **kwargs):
-        raise NotImplementedError()
+        return super().__call__(f, **kwargs)
+
+    @__call__.register
+    def _(self, f: pn.Function, /) -> pn.Function:
+        import scipy.integrate
+
+        def _int_f(b):
+            unsqueeze = False
+
+            if b.size == 1 and b.ndim == 1:
+                unsqueeze = True
+                b = b[0]
+
+            assert b.shape == ()
+
+            int_f = np.asarray(scipy.integrate.quad(f, a=self.lower_bound, b=b)[0])
+
+            if unsqueeze:
+                return int_f[None]
+
+            return int_f
+
+        return pn.LambdaFunction(
+            _int_f,
+            input_shape=(),
+            output_shape=(),
+        )
 
 
 @UndefinedLebesgueIntegral.__call__.register  # pylint: disable=no-member
