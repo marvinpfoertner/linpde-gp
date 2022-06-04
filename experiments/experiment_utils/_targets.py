f0f2c2ea04e555c2641a3bdff7bf4cd3381a8e7b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-04 16:30:35 +0200

    Add `research_project` target to `experiment_utils`

--- experiments/experiment_utils/_targets.py
+++ experiments/experiment_utils/_targets.py
@@ -3,6 +3,7 @@ from tueplots import bundles
 from ._tueplots_bundles import jmlr as jmlr_bundle
 
 _tueplots_bundles = {
-    "jmlr": jmlr_bundle,
     "imprs_2022": bundles.beamer_moml,
+    "jmlr": jmlr_bundle,
+    "research_project": jmlr_bundle,
 }
