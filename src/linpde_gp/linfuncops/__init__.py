bb14523ee0a032e6ba2815f1b799709cf1951d36
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-04 16:45:12 +0200

    `LinearFunctional` and arithmetic

--- src/linpde_gp/linfuncops/__init__.py
+++ src/linpde_gp/linfuncops/__init__.py
@@ -1,4 +1,9 @@
 from . import diffops
-from ._arithmetic import ScaledLinearFunctionOperator, SumLinearFunctionOperator
-from ._linfuncop import LinearFunctionOperator
+from ._arithmetic import (
+    ScaledLinearFunctional,
+    ScaledLinearFunctionOperator,
+    SumLinearFunctional,
+    SumLinearFunctionOperator,
+)
+from ._linfuncop import LinearFunctional, LinearFunctionOperator
 from .diffops import LambdaLinearDifferentialOperator, LinearDifferentialOperator
--- src/linpde_gp/linfuncops/_arithmetic.py
+++ src/linpde_gp/linfuncops/_arithmetic.py
@@ -5,7 +5,7 @@ import numpy as np
 import probnum as pn
 from probnum.typing import ScalarLike, ScalarType
 
-from ._linfuncop import LinearFunctionOperator
+from ._linfuncop import LinearFunctional, LinearFunctionOperator
 
 
 class ScaledLinearFunctionOperator(LinearFunctionOperator):
@@ -80,6 +80,83 @@ class SumLinearFunctionOperator(LinearFunctionOperator):
         super().__init__(
             input_shapes=(input_domain_shape, input_codomain_shape),
             output_shapes=(output_domain_shape, output_codomain_shape),
+        )
+
+    @functools.singledispatchmethod
+    def __call__(self, f, /, **kwargs):
+        return functools.reduce(
+            operator.add, (summand(f, **kwargs) for summand in self._summands)
+        )
+
+    @__call__.register
+    def _(self, randproc: pn.randprocs.RandomProcess, /) -> pn.randprocs.RandomProcess:
+        return super().__call__(randproc)
+
+
+class ScaledLinearFunctional(LinearFunctional):
+    def __init__(self, linfunctl: LinearFunctional, scalar: ScalarLike) -> None:
+        self._linfunctl = linfunctl
+
+        super().__init__(
+            input_shapes=self._linfunctl.input_shapes,
+            output_shape=self._linfunctl.output_shape,
+        )
+
+        if not np.ndim(scalar) == 0:
+            raise ValueError()
+
+        self._scalar = np.asarray(scalar, dtype=np.double)
+
+    @property
+    def linfunctl(self) -> LinearFunctional:
+        return self._linfunctl
+
+    @property
+    def scalar(self) -> ScalarType:
+        return self._scalar
+
+    @functools.singledispatchmethod
+    def __call__(self, f, /, **kwargs):
+        return self._scalar * self._linfunctl(f, **kwargs)
+
+    # TODO: Only need until GPs can be scaled
+    @__call__.register
+    def _(
+        self, gp: pn.randprocs.GaussianProcess, /, **kwargs
+    ) -> pn.randprocs.GaussianProcess:
+        return super().__call__(gp, **kwargs)
+
+    def __rmul__(self, other) -> LinearFunctional:
+        if np.ndim(other) == 0:
+            return ScaledLinearFunctional(
+                linfunctl=self._linfunctl,
+                scalar=np.asarray(other) * self._scalar,
+            )
+
+        return super().__rmul__(other)
+
+
+class SumLinearFunctional(LinearFunctional):
+    def __init__(self, *summands: LinearFunctional) -> None:
+        self._summands = tuple(summands)
+
+        input_domain_shape = self._summands[0].input_domain_shape
+        input_codomain_shape = self._summands[0].input_codomain_shape
+        output_shape = self._summands[0].output_shape
+
+        assert all(
+            summand.input_domain_shape == input_domain_shape
+            for summand in self._summands
+        )
+        assert all(
+            summand.input_codomain_shape == input_codomain_shape
+            for summand in self._summands
+        )
+        assert all(summand.output_shape == output_shape for summand in self._summands)
+
+        super().__init__(
+            input_shapes=(input_domain_shape, input_codomain_shape),
+            output_shape=output_shape,
         )
 
     @functools.singledispatchmethod
