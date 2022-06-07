604bed1e6bf024376021c21d9ca32d183c62bd38
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-07 19:26:35 +0200

    crosscov does not inherit from Function anymore

--- src/linpde_gp/randprocs/crosscov/_pv_crosscov.py
+++ src/linpde_gp/randprocs/crosscov/_pv_crosscov.py
@@ -1,18 +1,17 @@
 from __future__ import annotations
 
+import abc
 import functools
 import operator
 from typing import Type
 
+from jax import numpy as jnp
 import numpy as np
 import probnum as pn
-from probnum.typing import ShapeLike, ShapeType
-
-from linpde_gp import functions
+from probnum.typing import ArrayLike, ShapeLike, ShapeType
 
 
-# TODO: This should not inherit from `Function`
-class ProcessVectorCrossCovariance(functions.JaxFunction):
+class ProcessVectorCrossCovariance(abc.ABC):
     def __init__(
         self,
         randproc_input_shape: ShapeLike,
@@ -25,15 +24,6 @@ class ProcessVectorCrossCovariance(functions.JaxFunction):
         self._randvar_shape = pn.utils.as_shape(randvar_shape)
 
         self._reverse = bool(reverse)
-
-        super().__init__(
-            input_shape=randproc_input_shape,
-            output_shape=(
-                self._randvar_shape + self._randproc_output_shape
-                if self._reverse
-                else self._randproc_output_shape + self._randvar_shape
-            ),
-        )
 
     @property
     def randproc_input_shape(self) -> ShapeType:
@@ -66,6 +56,69 @@ class ProcessVectorCrossCovariance(functions.JaxFunction):
     @property
     def reverse(self) -> bool:
         return self._reverse
+
+    def __call__(self, x: ArrayLike) -> np.ndarray:
+        x = np.asarray(x)
+
+        # Shape checking
+        if x.shape[x.ndim - self.randproc_input_ndim :] != self.randproc_input_shape:
+            err_msg = (
+                "The shape of the input array must match the `randproc_input_shape` "
+                f"`{self.randproc_input_shape}` of the function along its last "
+                f"dimensions, but an array with shape `{x.shape}` was given."
+            )
+
+            raise ValueError(err_msg)
+
+        batch_shape = x.shape[: x.ndim - self.randproc_input_ndim]
+
+        fx = self._evaluate(x)
+
+        if self.reverse:
+            assert fx.shape == (
+                self.randvar_shape + batch_shape + self.randproc_output_shape
+            )
+        else:
+            assert fx.shape == (
+                batch_shape + self.randproc_output_shape + self.randvar_shape
+            )
+
+        return fx
+
+    @abc.abstractmethod
+    def _evaluate(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    def jax(self, x: ArrayLike) -> jnp.ndarray:
+        x = jnp.asarray(x)
+
+        if x.shape[x.ndim - self.randproc_input_ndim :] != self.randproc_input_shape:
+            err_msg = (
+                "The shape of the input array must match the `randproc_input_shape` "
+                f"`{self.randproc_input_shape}` of the function along its last "
+                f"dimensions, but an array with shape `{x.shape}` was given."
+            )
+
+            raise ValueError(err_msg)
+
+        batch_shape = x.shape[: x.ndim - self.randproc_input_ndim]
+
+        fx = self._evaluate_jax(x)
+
+        if self.reverse:
+            assert fx.shape == (
+                self.randvar_shape + batch_shape + self.randproc_output_shape
+            )
+        else:
+            assert fx.shape == (
+                batch_shape + self.randproc_output_shape + self.randvar_shape
+            )
+
+        return fx
+
+    @abc.abstractmethod
+    def _evaluate_jax(self, x: jnp.ndarray) -> jnp.ndarray:
+        pass
 
     def __neg__(self):
         return -1.0 * self
