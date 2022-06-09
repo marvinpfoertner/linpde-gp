0bdf734f72141051c48d448055d7a385464dbf28
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-09 19:13:32 +0200

    Initial slides of Master thesis presentation

--- experiments/experiment_utils/_targets.py
+++ experiments/experiment_utils/_targets.py
@@ -1,9 +1,10 @@
 from tueplots import bundles
 
-from ._tueplots_bundles import jmlr as jmlr_bundle
+from ._tueplots_bundles import beamer_moml as beamer_moml_bundle, jmlr as jmlr_bundle
 
 _tueplots_bundles = {
     "imprs_2022": bundles.beamer_moml,
     "jmlr": jmlr_bundle,
     "research_project": jmlr_bundle,
+    "thesis_talk": beamer_moml_bundle,
 }
--- experiments/experiment_utils/_tueplots_bundles.py
+++ experiments/experiment_utils/_tueplots_bundles.py
@@ -1,4 +1,4 @@
-from tueplots import figsizes, fonts, fontsizes
+from tueplots import bundles, figsizes, fonts, fontsizes
 
 
 def jmlr(*, rel_width=1.0, nrows=1, ncols=1, family="serif", **kwargs):
@@ -11,3 +11,10 @@ def jmlr(*, rel_width=1.0, nrows=1, ncols=1, family="serif", **kwargs):
     font_config = fonts.jmlr2001_tex(family=family)
     fontsize_config = fontsizes.jmlr2001()
     return {**font_config, **size, **fontsize_config}
+
+
+def beamer_moml(*, rel_width=1.0, rel_height=0.8, nrows=1):
+    return bundles.beamer_moml(
+        rel_width=rel_width,
+        rel_height=rel_height,
+    )
