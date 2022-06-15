20fc5008be1c6e8e544c2a8e2fa289cb8e0bc195
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-15 17:02:12 +0200

    Matern Laplacian Cross kernel

--- src/linpde_gp/randprocs/kernels/diffops/_matern_laplacian.py
+++ src/linpde_gp/randprocs/kernels/diffops/_matern_laplacian.py
@@ -8,7 +8,7 @@ import numpy as np
 from linpde_gp.linfuncops import diffops
 
 from .._jax import JaxKernel
-from .._matern import Matern
+from .._matern import Matern, ProductMatern
 from .._stationary import JaxStationaryMixin
 from ._matern_directional_derivative import Matern_Identity_DirectionalDerivative
 
@@ -17,6 +17,9 @@ class Matern_Identity_Laplacian(JaxKernel, JaxStationaryMixin):
     def __init__(self, matern: Matern, reverse: bool = True):
 
         self._matern = matern
+
+        if self._matern.input_ndim != 0:
+            raise ValueError()
 
         super().__init__(self._matern.input_shape, output_shape=())
 
@@ -64,6 +67,89 @@ class Matern_Identity_Laplacian(JaxKernel, JaxStationaryMixin):
 def _(self, k: Matern, /, *, argnum: int = 0):  # pylint: disable=unused-argument
     return Matern_Identity_Laplacian(
         matern=k,
+        reverse=(argnum == 0),
+    )
+
+
+class ProductMatern_Identity_Laplacian(JaxKernel, JaxStationaryMixin):
+    def __init__(self, prod_matern: ProductMatern, reverse: bool = True):
+        self._prod_matern = prod_matern
+
+        super().__init__(self._prod_matern.input_shape, output_shape=())
+
+        self._reverse = bool(reverse)
+
+    @property
+    def prod_matern(self) -> Matern:
+        return self._prod_matern
+
+    @property
+    def reverse(self) -> bool:
+        return self._reverse
+
+    def _evaluate(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> np.ndarray:
+        return np.sum(
+            np.prod(
+                np.where(
+                    np.eye(self.input_shape[0], dtype=np.bool),
+                    self._evaluate_factors(x0, x1)[..., None],
+                    self._prod_matern._evaluate_factors(x0, x1)[..., None],
+                ),
+                axis=-1,
+            ),
+            axis=-1,
+        )
+
+    def _evaluate_factors(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> np.ndarray:
+        if x1 is None:
+            scaled_dists = np.zeros_like(x0)
+        else:
+            scaled_dists = self._prod_matern._scale_factors * np.abs(x0 - x1)
+
+        if self._prod_matern.p == 3:
+            return (
+                self._prod_matern._scale_factors**2
+                * np.exp(-scaled_dists)
+                * ((1.0 / 15.0 * scaled_dists**2 - 0.2) * scaled_dists - 0.2)
+            )
+
+        raise ValueError()
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def _evaluate_jax(self, x0: jnp.ndarray, x1: Optional[jnp.ndarray]) -> jnp.ndarray:
+        return jnp.sum(
+            jnp.prod(
+                self._prod_matern._evaluate_factors_jax(x0, x1)[:, None],
+                axis=-1,
+                where=~np.eye(self.input_shape[0], dtype=np.bool),
+            )
+            * self._evaluate_factors_jax(x0, x1),
+            axis=-1,
+        )
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def _evaluate_factors_jax(
+        self, x0: jnp.ndarray, x1: Optional[jnp.ndarray]
+    ) -> jnp.ndarray:
+        if x1 is None:
+            scaled_dists = jnp.zeros_like(x0, shape=x0.shape)
+        else:
+            scaled_dists = self._prod_matern._scale_factors * jnp.abs(x0 - x1)
+
+        if self._prod_matern.p == 3:
+            return (
+                self._prod_matern._scale_factors**2
+                * jnp.exp(-scaled_dists)
+                * ((1.0 / 15.0 * scaled_dists**2 - 0.2) * scaled_dists - 0.2)
+            )
+
+        raise ValueError()
+
+
+@diffops.Laplacian.__call__.register  # pylint: disable=no-member
+def _(self, k: ProductMatern, /, *, argnum: int = 0):  # pylint: disable=unused-argument
+    return ProductMatern_Identity_Laplacian(
+        prod_matern=k,
         reverse=(argnum == 0),
     )
 
