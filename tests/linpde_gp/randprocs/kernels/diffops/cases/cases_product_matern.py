0984860f865609300c06d5ff70ba81b16985925a
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-16 16:11:55 +0200

    DirectionalDerivative applied to ProductMatern

--- tests/linpde_gp/randprocs/kernels/diffops/cases/cases_product_matern.py
+++ tests/linpde_gp/randprocs/kernels/diffops/cases/cases_product_matern.py
@@ -1,3 +1,4 @@
+import numpy as np
 from probnum.typing import ShapeType
 
 from pytest_cases import parametrize
@@ -7,6 +8,63 @@ import linpde_gp
 from ._test_case import KernelLinFuncOpTestCase
 
 input_shapes = ((1,), (2,))
+
+
+@parametrize(
+    input_shape=input_shapes,
+)
+def case_identity_directional_derivative(
+    input_shape: ShapeType,
+) -> KernelLinFuncOpTestCase:
+    rng = np.random.default_rng(390852098)
+
+    direction = rng.standard_normal(size=input_shape)
+    direction /= np.sqrt(np.sum(direction**2))
+
+    return KernelLinFuncOpTestCase(
+        k=linpde_gp.randprocs.kernels.ProductMatern(input_shape=input_shape),
+        L0=None,
+        L1=linpde_gp.linfuncops.diffops.DirectionalDerivative(direction),
+    )
+
+
+@parametrize(
+    input_shape=input_shapes,
+)
+def case_directional_derivative_identity(
+    input_shape: ShapeType,
+) -> KernelLinFuncOpTestCase:
+    rng = np.random.default_rng(390852098)
+
+    direction = rng.standard_normal(size=input_shape)
+    direction /= np.sqrt(np.sum(direction**2))
+
+    return KernelLinFuncOpTestCase(
+        k=linpde_gp.randprocs.kernels.ProductMatern(input_shape=input_shape),
+        L0=linpde_gp.linfuncops.diffops.DirectionalDerivative(direction),
+        L1=None,
+    )
+
+
+@parametrize(
+    input_shape=input_shapes,
+)
+def case_directional_derivative_directional_derivative(
+    input_shape: ShapeType,
+) -> KernelLinFuncOpTestCase:
+    rng = np.random.default_rng(390852098)
+
+    direction0 = rng.standard_normal(size=input_shape)
+    direction0 /= np.sqrt(np.sum(direction0**2))
+
+    direction1 = rng.standard_normal(size=input_shape)
+    direction1 /= np.sqrt(np.sum(direction1**2))
+
+    return KernelLinFuncOpTestCase(
+        k=linpde_gp.randprocs.kernels.ProductMatern(input_shape=input_shape),
+        L0=linpde_gp.linfuncops.diffops.DirectionalDerivative(direction0),
+        L1=linpde_gp.linfuncops.diffops.DirectionalDerivative(direction1),
+    )
 
 
 @parametrize(
