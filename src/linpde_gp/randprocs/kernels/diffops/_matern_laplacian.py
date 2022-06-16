ce634fbfd4b88e2e61f13c44f2a00f3728b09dbc
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-06-16 10:53:28 +0200

    ProductMatern both-sided Laplacian

--- src/linpde_gp/randprocs/kernels/diffops/_matern_laplacian.py
+++ src/linpde_gp/randprocs/kernels/diffops/_matern_laplacian.py
@@ -91,9 +91,9 @@ class ProductMatern_Identity_Laplacian(JaxKernel, JaxStationaryMixin):
         return np.sum(
             np.prod(
                 np.where(
-                    np.eye(self.input_shape[0], dtype=np.bool),
-                    self._evaluate_factors(x0, x1)[..., None],
-                    self._prod_matern._evaluate_factors(x0, x1)[..., None],
+                    np.eye(self.input_shape[0], dtype=np.bool_),
+                    self._evaluate_factors(x0, x1)[..., None, :],
+                    self._prod_matern._evaluate_factors(x0, x1)[..., None, :],
                 ),
                 axis=-1,
             ),
@@ -119,11 +119,13 @@ class ProductMatern_Identity_Laplacian(JaxKernel, JaxStationaryMixin):
     def _evaluate_jax(self, x0: jnp.ndarray, x1: Optional[jnp.ndarray]) -> jnp.ndarray:
         return jnp.sum(
             jnp.prod(
-                self._prod_matern._evaluate_factors_jax(x0, x1)[:, None],
+                jnp.where(
+                    jnp.eye(self.input_shape[0], dtype=jnp.bool_),
+                    self._evaluate_factors(x0, x1)[..., None, :],
+                    self._prod_matern._evaluate_factors(x0, x1)[..., None, :],
+                ),
                 axis=-1,
-                where=~np.eye(self.input_shape[0], dtype=np.bool),
-            )
-            * self._evaluate_factors_jax(x0, x1),
+            ),
             axis=-1,
         )
 
@@ -204,6 +206,111 @@ class Matern_Laplacian_Laplacian(JaxKernel, JaxStationaryMixin):
 def _(self, k: Matern_Identity_Laplacian, /, *, argnum: int = 0):
     if (argnum == 0 and not k.reverse) or (argnum == 1 and k.reverse):
         return Matern_Laplacian_Laplacian(matern=k.matern)
+
+    return super(diffops.Laplacian, self).__call__(k, argnum=argnum)
+
+
+class ProductMatern_Laplacian_Laplacian(JaxKernel, JaxStationaryMixin):
+    def __init__(self, prod_matern: ProductMatern):
+        self._prod_matern = prod_matern
+        self._prod_matern_id_lap = ProductMatern_Identity_Laplacian(
+            self._prod_matern,
+            reverse=False,
+        )
+
+        super().__init__(self._prod_matern.input_shape, output_shape=())
+
+    @property
+    def prod_matern(self) -> ProductMatern:
+        return self._prod_matern
+
+    def _evaluate(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> np.ndarray:
+        idx_equal_mask = np.eye(self.input_shape[0], dtype=np.bool)
+
+        ks_x0_x1 = np.where(
+            idx_equal_mask[None, :, :] ^ idx_equal_mask[:, None, :],
+            self._prod_matern_id_lap._evaluate_factors(x0, x1)[..., None, None, :],
+            self._prod_matern._evaluate_factors(x0, x1)[..., None, None, :],
+        )
+
+        ks_x0_x1 = np.where(
+            idx_equal_mask[None, :, :] & idx_equal_mask[:, None, :],
+            self._evaluate_factors(x0, x1)[..., None, None, :],
+            ks_x0_x1,
+        )
+
+        return np.sum(
+            np.prod(ks_x0_x1, axis=-1),
+            axis=(-2, -1),
+        )
+
+    def _evaluate_factors(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> np.ndarray:
+        if x1 is None:
+            scaled_dists = np.zeros_like(x0)
+        else:
+            scaled_dists = self._prod_matern._scale_factors * np.abs(x0 - x1)
+
+        if self._prod_matern.p == 3:
+            return (
+                self._prod_matern._scale_factors**4
+                * np.exp(-scaled_dists)
+                * (
+                    ((1.0 / 15.0 * scaled_dists - 0.4) * scaled_dists + 0.2)
+                    * scaled_dists
+                    + 0.2
+                )
+            )
+
+        raise ValueError()
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def _evaluate_jax(self, x0: jnp.ndarray, x1: Optional[jnp.ndarray]) -> jnp.ndarray:
+        idx_equal_mask = np.eye(self.input_shape[0], dtype=np.bool)
+
+        ks_x0_x1 = np.where(
+            idx_equal_mask[None, :, :] ^ idx_equal_mask[:, None, :],
+            self._prod_matern_id_lap._evaluate_factors(x0, x1)[..., None, None, :],
+            self._prod_matern._evaluate_factors(x0, x1)[..., None, None, :],
+        )
+
+        ks_x0_x1 = np.where(
+            idx_equal_mask[None, :, :] & idx_equal_mask[:, None, :],
+            self._evaluate_factors(x0, x1)[..., None, None, :],
+            ks_x0_x1,
+        )
+
+        return np.sum(
+            np.prod(ks_x0_x1, axis=-1),
+            axis=(-2, -1),
+        )
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def _evaluate_factors_jax(
+        self, x0: jnp.ndarray, x1: Optional[jnp.ndarray]
+    ) -> jnp.ndarray:
+        if x1 is None:
+            scaled_dists = jnp.zeros_like(x0)
+        else:
+            scaled_dists = self._prod_matern._scale_factors * jnp.abs(x0 - x1)
+
+        if self._prod_matern.p == 3:
+            return (
+                self._prod_matern._scale_factors**4
+                * jnp.exp(-scaled_dists)
+                * (
+                    ((1.0 / 15.0 * scaled_dists - 0.4) * scaled_dists + 0.2)
+                    * scaled_dists
+                    + 0.2
+                )
+            )
+
+        raise ValueError()
+
+
+@diffops.Laplacian.__call__.register  # pylint: disable=no-member
+def _(self, k: ProductMatern_Identity_Laplacian, /, *, argnum: int = 0):
+    if (argnum == 0 and not k.reverse) or (argnum == 1 and k.reverse):
+        return ProductMatern_Laplacian_Laplacian(prod_matern=k.prod_matern)
 
     return super(diffops.Laplacian, self).__call__(k, argnum=argnum)
 
