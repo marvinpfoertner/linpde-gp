109d52183ac5b94c6067fb271fdd86b0f14d0c01
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-02-11 18:09:32 +0100

    Add consistency checks in `BoundaryValueProblem`

--- src/linpde_gp/problems/pde/_bvp.py
+++ src/linpde_gp/problems/pde/_bvp.py
@@ -24,3 +24,22 @@ class BoundaryValueProblem:
     @property
     def domain(self):
         return self.pde.domain
+
+    def __post_init__(self):
+        for boundary_condition in self.boundary_conditions:
+            if boundary_condition.boundary.shape != self.domain.shape:
+                raise ValueError(
+                    "The shape of the boundary must be equal to the shape of the domain"
+                )
+
+        if self.solution.input_shape != self.domain.shape:
+            raise ValueError(
+                "The input shape of the solution function should be equal to the shape "
+                "of the domain."
+            )
+
+        if self.solution.output_shape != self.pde.diffop.input_codomain_shape:
+            raise ValueError(
+                "The output shape of the solution function should be equal to the "
+                "output shape of the differential operator's input function."
+            )
