e6c8d2e900df10c44f1018067b73267aa5fc3965
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-03-04 14:10:07 +0100

    Matérn-1/2 kernel embedding

--- src/linpde_gp/randprocs/crosscov/linfunctls/integrals/_matern_lebesgue.py
+++ src/linpde_gp/randprocs/crosscov/linfunctls/integrals/_matern_lebesgue.py
@@ -41,54 +41,66 @@ class Matern_Identity_LebesgueIntegral(_pv_crosscov.ProcessVectorCrossCovariance
         a, b = self._integral.domain
 
         # adapted from `probnum.quad.kernel_embeddings._matern_lebesgue`
-        return (
-            1.0
-            / (105.0 * ell**2)
-            * (
-                96.0 * np.sqrt(7.0) * ell**3
-                - np.exp(np.sqrt(7.0) * (x - b) / ell)
-                * (
-                    48.0 * np.sqrt(7.0) * ell**3
-                    - 231.0 * ell**2 * (x - b)
-                    + 63.0 * np.sqrt(7.0) * ell * (x - b) ** 2
-                    - 49.0 * (x - b) ** 3
+        match self.matern.p:
+            case 0:
+                return ell * (2.0 - np.exp((a - x) / ell) - np.exp((x - b) / ell))
+            case 3:
+                return (
+                    1.0
+                    / (105.0 * ell**2)
+                    * (
+                        96.0 * np.sqrt(7.0) * ell**3
+                        - np.exp(np.sqrt(7.0) * (x - b) / ell)
+                        * (
+                            48.0 * np.sqrt(7.0) * ell**3
+                            - 231.0 * ell**2 * (x - b)
+                            + 63.0 * np.sqrt(7.0) * ell * (x - b) ** 2
+                            - 49.0 * (x - b) ** 3
+                        )
+                        - np.exp(np.sqrt(7.0) * (a - x) / ell)
+                        * (
+                            48.0 * np.sqrt(7.0) * ell**3
+                            + 231.0 * ell**2 * (x - a)
+                            + 63.0 * np.sqrt(7.0) * ell * (x - a) ** 2
+                            + 49.0 * (x - a) ** 3
+                        )
+                    )
                 )
-                - np.exp(np.sqrt(7.0) * (a - x) / ell)
-                * (
-                    48.0 * np.sqrt(7.0) * ell**3
-                    + 231.0 * ell**2 * (x - a)
-                    + 63.0 * np.sqrt(7.0) * ell * (x - a) ** 2
-                    + 49.0 * (x - a) ** 3
-                )
-            )
-        )
+
+        raise NotImplementedError
 
     def _evaluate_jax(self, x: jnp.ndarray) -> jnp.ndarray:
         ell = self._matern.lengthscales
         a, b = self._integral.domain
 
         # adapted from `probnum.quad.kernel_embeddings._matern_lebesgue`
-        return (
-            1.0
-            / (105.0 * ell**2)
-            * (
-                96.0 * jnp.sqrt(7.0) * ell**3
-                - jnp.exp(jnp.sqrt(7.0) * (x - b) / ell)
-                * (
-                    48.0 * jnp.sqrt(7.0) * ell**3
-                    - 231.0 * ell**2 * (x - b)
-                    + 63.0 * jnp.sqrt(7.0) * ell * (x - b) ** 2
-                    - 49.0 * (x - b) ** 3
+        match self.matern.p:
+            case 0:
+                return ell * (2.0 - jnp.exp((a - x) / ell) - jnp.exp((x - b) / ell))
+            case 3:
+                return (
+                    1.0
+                    / (105.0 * ell**2)
+                    * (
+                        96.0 * jnp.sqrt(7.0) * ell**3
+                        - jnp.exp(jnp.sqrt(7.0) * (x - b) / ell)
+                        * (
+                            48.0 * jnp.sqrt(7.0) * ell**3
+                            - 231.0 * ell**2 * (x - b)
+                            + 63.0 * jnp.sqrt(7.0) * ell * (x - b) ** 2
+                            - 49.0 * (x - b) ** 3
+                        )
+                        - jnp.exp(jnp.sqrt(7.0) * (a - x) / ell)
+                        * (
+                            48.0 * jnp.sqrt(7.0) * ell**3
+                            + 231.0 * ell**2 * (x - a)
+                            + 63.0 * jnp.sqrt(7.0) * ell * (x - a) ** 2
+                            + 49.0 * (x - a) ** 3
+                        )
+                    )
                 )
-                - jnp.exp(jnp.sqrt(7.0) * (a - x) / ell)
-                * (
-                    48.0 * jnp.sqrt(7.0) * ell**3
-                    + 231.0 * ell**2 * (x - a)
-                    + 63.0 * jnp.sqrt(7.0) * ell * (x - a) ** 2
-                    + 49.0 * (x - a) ** 3
-                )
-            )
-        )
+
+        raise NotImplementedError
 
 
 @linfunctls.LebesgueIntegral.__call__.register(  # pylint: disable=no-member
@@ -101,28 +113,37 @@ def _(self, pv_crosscov: Matern_Identity_LebesgueIntegral, /) -> ScalarType:
     # adapted from `probnum.quad.kernel_embeddings._matern_lebesgue`
     ell = pv_crosscov.matern.lengthscales
     a, b = self.domain
-    c = np.sqrt(7.0) * (b - a)
 
-    return (
-        1.0
-        / (105.0 * ell)
-        * (
-            2.0
-            * np.exp(-c / ell)
-            * (
-                7.0 * np.sqrt(7.0) * (b**3 - a**3)
-                + 84.0 * b**2 * ell
-                + 57.0 * np.sqrt(7.0) * b * ell**2
-                + 105.0 * ell**3
-                + 21.0 * a**2 * (np.sqrt(7.0) * b + 4.0 * ell)
-                - 3.0
-                * a
+    match pv_crosscov.matern.p:
+        case 0:
+            r = b - a
+
+            return 2.0 * ell * (r + ell * (np.exp(-r / ell) - 1.0))
+        case 3:
+            c = np.sqrt(7.0) * (b - a)
+
+            return (
+                1.0
+                / (105.0 * ell)
                 * (
-                    7.0 * np.sqrt(7.0) * b**2
-                    + 56.0 * b * ell
-                    + 19.0 * np.sqrt(7.0) * ell**2
+                    2.0
+                    * np.exp(-c / ell)
+                    * (
+                        7.0 * np.sqrt(7.0) * (b**3 - a**3)
+                        + 84.0 * b**2 * ell
+                        + 57.0 * np.sqrt(7.0) * b * ell**2
+                        + 105.0 * ell**3
+                        + 21.0 * a**2 * (np.sqrt(7.0) * b + 4.0 * ell)
+                        - 3.0
+                        * a
+                        * (
+                            7.0 * np.sqrt(7.0) * b**2
+                            + 56.0 * b * ell
+                            + 19.0 * np.sqrt(7.0) * ell**2
+                        )
+                    )
+                    - 6.0 * ell**2 * (35.0 * ell - 16.0 * c)
                 )
             )
-            - 6.0 * ell**2 * (35.0 * ell - 16.0 * c)
-        )
-    )
+
+    raise NotImplementedError
