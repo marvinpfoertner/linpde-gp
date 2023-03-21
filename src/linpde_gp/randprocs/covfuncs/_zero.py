2146f12cc52f25486ec52f6b7ef33b0ff09ea6ad
Tim Weiland <tim@weiland-lahnstein.de>
2023-03-21 12:56:17 +0100

    Linop impl for Zero CovarianceFunction

--- src/linpde_gp/randprocs/covfuncs/_zero.py
+++ src/linpde_gp/randprocs/covfuncs/_zero.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from jax import numpy as jnp
 import numpy as np
+import probnum as pn
 from probnum.randprocs import covfuncs
 
 from ._jax import JaxCovarianceFunctionMixin
@@ -25,3 +27,10 @@ class Zero(JaxCovarianceFunctionMixin, covfuncs.CovarianceFunction):
             broadcast_batch_shape + self.output_shape_0 + self.output_shape_1,
             dtype=np.result_type(x0, x1),
         )
+
+    def _evaluate_linop(self, x0: np.ndarray, x1: Optional[np.ndarray]) -> pn.linops.LinearOperator:
+        shape = (
+            self.output_size_0 * x0.shape[0],
+            self.output_size_1 * (x1.shape[0] if x1 is not None else x0.shape[0]),
+        )
+        return pn.linops.Zero(shape, np.promote_types(x0.dtype, x1.dtype))
