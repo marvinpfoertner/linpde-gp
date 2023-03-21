5ca26aece85d5d8144f212d7b2e5c04f03b971d2
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-03-21 17:53:19 +0100

    Remove `linalg`

--- src/linpde_gp/linalg/solvers/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import belief_updates, beliefs, observation_ops, policies, stopping_criteria
-from ._bayescg import BayesCG, bayescg
-from ._cg import ConjugateGradients
-from ._probabilistic_linear_solver import ProbabilisticLinearSolver
-from ._problinsolve import problinsolve
