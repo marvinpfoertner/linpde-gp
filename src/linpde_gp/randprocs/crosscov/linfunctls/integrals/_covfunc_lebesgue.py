5a065b8d01357ea93b87f9cacda372938720ef4b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-05-25 17:44:41 +0200

    Generic kL' / Lk cross covariance implementation

--- src/linpde_gp/randprocs/crosscov/linfunctls/integrals/_covfunc_lebesgue.py
+++ src/linpde_gp/randprocs/crosscov/linfunctls/integrals/_covfunc_lebesgue.py
@@ -8,7 +8,7 @@ import scipy.integrate
 
 from linpde_gp import linfunctls
 
-from ... import _pv_crosscov
+from .._base import LinearFunctionalProcessVectorCrossCovariance
 
 
 class np_vectorize_method(np.vectorize):
@@ -20,35 +20,33 @@ class np_vectorize_method(np.vectorize):
 
 
 class CovarianceFunction_Identity_LebesgueIntegral(
-    _pv_crosscov.ProcessVectorCrossCovariance
+    LinearFunctionalProcessVectorCrossCovariance
 ):
     def __init__(
         self,
-        k: pn.randprocs.covfuncs.CovarianceFunction,
-        L: linfunctls.LebesgueIntegral,
+        covfunc: pn.randprocs.covfuncs.CovarianceFunction,
+        integral: linfunctls.LebesgueIntegral,
         reverse: bool = False,
     ):
-        randproc_output_shape = k.output_shape_1 if reverse else k.output_shape_0
-
-        if randproc_output_shape != ():
+        if integral.output_shape != ():
             raise NotImplementedError()
 
         super().__init__(
-            randproc_input_shape=k.input_shape_1 if reverse else k.input_shape_0,
-            randproc_output_shape=randproc_output_shape,
-            randvar_shape=(),
+            covfunc=covfunc,
+            linfunctl=integral,
             reverse=reverse,
         )
 
-        self._k = k
-        self._L = L
+    @property
+    def integral(self) -> linfunctls.LebesgueIntegral:
+        return self.linfunctl
 
     @functools.partial(np_vectorize_method, excluded={0})
     def _evaluate(self, x: np.ndarray) -> np.ndarray:
         return scipy.integrate.quad(
-            lambda t: self._k(x, t),
-            a=self._L.domain[0],
-            b=self._L.domain[1],
+            lambda t: self.covfunc(x, t),
+            a=self.integral.domain[0],
+            b=self.integral.domain[1],
         )[0]
 
     def _evaluate_jax(self, x: jnp.ndarray) -> jnp.ndarray:
@@ -56,16 +54,16 @@ class CovarianceFunction_Identity_LebesgueIntegral(
 
 
 @linfunctls.LebesgueIntegral.__call__.register  # pylint: disable=no-member
-def _(self, pv_crosscov: CovarianceFunction_Identity_LebesgueIntegral, /):
-    if pv_crosscov.reverse:
-        L0 = pv_crosscov._L
-        L1 = self
-    else:
-        L0 = self
-        L1 = pv_crosscov._L
+def _(self, Lk_or_kL: CovarianceFunction_Identity_LebesgueIntegral, /):
+    if Lk_or_kL.reverse:  # Lk
+        integral0 = Lk_or_kL.integral
+        integral1 = self
+    else:  # kL'
+        integral0 = self
+        integral1 = Lk_or_kL.integral
 
     return scipy.integrate.dblquad(
-        lambda x1, x0: pv_crosscov._k(x0, x1),
-        *L0.domain,
-        *L1.domain,
+        lambda x1, x0: Lk_or_kL.covfunc(x0, x1),
+        *integral0.domain,
+        *integral1.domain,
     )[0]
