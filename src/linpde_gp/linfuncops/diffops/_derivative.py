f5d479e94770d4efe784537562cba656f215a8e3
Tim Weiland <tim@weiland-lahnstein.de>
2023-06-23 14:34:45 +0200

    Add coefficient representation to `Derivative`

--- src/linpde_gp/linfuncops/diffops/_derivative.py
+++ src/linpde_gp/linfuncops/diffops/_derivative.py
@@ -6,6 +6,7 @@ import probnum as pn
 
 import linpde_gp  # pylint: disable=unused-import # for type hints
 
+from ._coefficients import PartialDerivativeCoefficients
 from ._lindiffop import LinearDifferentialOperator
 
 
@@ -17,7 +18,10 @@ class Derivative(LinearDifferentialOperator):
         if order < 0:
             raise ValueError(f"Order must be >= 0, but got {order}.")
 
-        super().__init__(input_shapes=((), ()))
+        super().__init__(
+            coefficients=PartialDerivativeCoefficients({(): {(order,): 1.0}}, (), ()),
+            input_shapes=((), ()),
+        )
 
         self._order = order
 
