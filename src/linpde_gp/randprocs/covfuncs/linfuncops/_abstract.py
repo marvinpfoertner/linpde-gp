9e555874b61fdf60a03c8d09b0803165428de129
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-06-24 01:05:32 +0200

    `k` -> `covfunc`

--- src/linpde_gp/randprocs/covfuncs/linfuncops/_abstract.py
+++ src/linpde_gp/randprocs/covfuncs/linfuncops/_abstract.py
@@ -8,35 +8,39 @@ class CrossCovarianceFunction_Identity_LinearFunctionOperator(
 ):
     def __init__(
         self,
-        k: covfuncs.CovarianceFunction,
+        covfunc: covfuncs.CovarianceFunction,
         L: linfuncops.LinearFunctionOperator,
         reverse: bool = False,
     ):
-        self._k = k
+        self._covfunc = covfunc
         self._L = L
         self._reverse = bool(reverse)
 
         if self._L.input_domain_shape != (
-            self._k.input_shape_0 if self._reverse else self._k.input_shape_1
+            self._covfunc.input_shape_0
+            if self._reverse
+            else self._covfunc.input_shape_1
         ):
             raise ValueError()
 
         if self._L.input_codomain_shape != (
-            self._k.output_shape_0 if self._reverse else self._k.output_shape_1
+            self._covfunc.output_shape_0
+            if self._reverse
+            else self._covfunc.output_shape_1
         ):
             raise ValueError()
 
         if self._reverse:
             input_shape_0 = self._L.output_domain_shape
-            input_shape_1 = self._k.input_shape_1
+            input_shape_1 = self._covfunc.input_shape_1
 
             output_shape_0 = self._L.output_codomain_shape
-            output_shape_1 = self._k.output_shape_1
+            output_shape_1 = self._covfunc.output_shape_1
         else:
-            input_shape_0 = self._k.input_shape_0
+            input_shape_0 = self._covfunc.input_shape_0
             input_shape_1 = self._L.output_domain_shape
 
-            output_shape_0 = self._k.output_shape_0
+            output_shape_0 = self._covfunc.output_shape_0
             output_shape_1 = self._L.output_codomain_shape
 
         super().__init__(
@@ -47,8 +51,8 @@ class CrossCovarianceFunction_Identity_LinearFunctionOperator(
         )
 
     @property
-    def k(self) -> covfuncs.CovarianceFunction:
-        return self._k
+    def covfunc(self) -> covfuncs.CovarianceFunction:
+        return self._covfunc
 
     @property
     def L(self) -> linfuncops.LinearFunctionOperator:
@@ -66,24 +70,24 @@ class CrossCovarianceFunction_Identity_LinearFunctionOperator(
 class CovarianceFunction_LinearFunctionOperator(covfuncs.CovarianceFunction):
     def __init__(
         self,
-        k: covfuncs.CovarianceFunction,
+        covfunc: covfuncs.CovarianceFunction,
         L0: linfuncops.LinearFunctionOperator,
         L1: linfuncops.LinearFunctionOperator,
     ):
-        self._k = k
+        self._covfunc = covfunc
         self._L0 = L0
         self._L1 = L1
 
-        if self._L0.input_domain_shape != self._k.input_shape_0:
+        if self._L0.input_domain_shape != self._covfunc.input_shape_0:
             raise ValueError()
 
-        if self._L0.input_codomain_shape != self._k.output_shape_0:
+        if self._L0.input_codomain_shape != self._covfunc.output_shape_0:
             raise ValueError()
 
-        if self._L1.input_domain_shape != self._k.input_shape_1:
+        if self._L1.input_domain_shape != self._covfunc.input_shape_1:
             raise ValueError()
 
-        if self._L1.input_codomain_shape != self._k.output_shape_1:
+        if self._L1.input_codomain_shape != self._covfunc.output_shape_1:
             raise ValueError()
 
         super().__init__(
@@ -94,8 +98,8 @@ class CovarianceFunction_LinearFunctionOperator(covfuncs.CovarianceFunction):
         )
 
     @property
-    def k(self) -> covfuncs.CovarianceFunction:
-        return self._k
+    def covfunc(self) -> covfuncs.CovarianceFunction:
+        return self._covfunc
 
     @property
     def L0(self) -> linfuncops.LinearFunctionOperator:
